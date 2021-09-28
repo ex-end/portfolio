3c4dabe818fb7dd16f1d1f7373f0f69680daa4fd
ex-end <pauk-sanek@mail.ru>
2021-09-28 19:06:21 +0300

    test

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,12 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+if ('serviceWorker' in navigator) {
+    // Весь код регистрации у нас асинхронный.
+    navigator.serviceWorker.register('./sw.js')
+      .then(() => navigator.serviceWorker.ready.then((worker) => {
+        worker.sync.register('syncdata');
+      }))
+      .catch((err) => console.log(err));
+}
